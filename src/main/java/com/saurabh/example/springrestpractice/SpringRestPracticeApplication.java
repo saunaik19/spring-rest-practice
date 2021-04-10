b3df799b055986d7636cef30a1005a7190dea63e
saunaik19 <saunaik19@gmail.com>
2021-04-10 09:07:58 +0530

    First commit

---
+++ src/main/java/com/saurabh/example/springrestpractice/SpringRestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.example.springrestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestPracticeApplication.class, args);
+	}
+
+}
